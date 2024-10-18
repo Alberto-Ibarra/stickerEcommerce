f225ae6ddf88ce09ad7a600708e491b8e892b30b
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2024-10-17 21:24:07 -0500

    Added get one, delete, and update sticker to controller and routes

--- server/controllers/stickerContoller.js
+++ server/controllers/stickerContoller.js
@@ -1,3 +1,4 @@
+const { options } = require('joi');
 const Product = require('../models/Product');
 
 const getAllStickers = async (req, res) => {
@@ -5,8 +6,38 @@ const getAllStickers = async (req, res) => {
         const products = await Product.find();
         res.status(200).json(products);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching products', error})
+        res.status(500).json({ message: 'Error fetching products', error});
     }
 };
 
-module.exports = {getAllStickers}
+const getStickerById = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: "Error fetching product", error});
+    }
+}
+
+const findStickerByIdAndUpdate = async (req,res) => {
+    try {
+        const productId = req.params.id;
+        const updateProduct = await Product.findByIdAndUpdate(productId);
+        res.status(200).json({message: "Product updated!"}).json(updateProduct);
+    } catch (error) {
+        res.status(500).json({message: "Update failed"});
+    }
+}
+
+const deleteStickerById = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const deleteProduct = await Product.deleteOne({ _id: productId});
+        res.status(500).json({message: "Product deleted"});
+    } catch (error) {
+        res.status(500).json({message: "Delete failed"});
+    }
+}
+
+module.exports = {getAllStickers, getStickerById, deleteStickerById, findStickerByIdAndUpdate}
--- server/routes/stickerRoutes.js
+++ server/routes/stickerRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const stickerContoller = require("../controllers/stickerContoller");
 
-router.get("/", stickerContoller.getAllStickers)
+router.get("/", stickerContoller.getAllStickers);
+router.get("/product/:id", stickerContoller.getStickerById);
+router.delete("/product/:id", stickerContoller.deleteStickerById);
+router.put("/product/:id", stickerContoller.findStickerByIdAndUpdate);
 
 module.exports = router;
