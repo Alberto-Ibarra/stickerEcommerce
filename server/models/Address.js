c4b5e8b5fdfea2b8172fca76f1c8fa7044b6c6d1
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2024-10-23 21:54:36 -0500

    clean up

--- server/models/Address.js
+++ server/models/Address.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const addressSchema = mongoose.Schema({
-    user: {type: String, required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     street: String,
     unit: String,
     city: String,
